8d4787fca2969b593cac7c5cf42bc75c3a0fac67
JenRed12 <jennifer.frisk@tealium.com>
2013-04-10 14:54:27 -0700

    add start.ini configs

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,7 +20,12 @@ default["jetty"]["host"] = "0.0.0.0"
 default["jetty"]["port"] = "8085"
 default["jetty"]["no_start"] = 0
 default["jetty"]["jetty_args"] = ""
-default["jetty"]["java_options"] = %w{--exec -Xmx2048m}
+default["jetty"]["java_options"] = %w{
+  --exec 
+  -Xmx2048m 
+  -Xmn512m 
+  XX:+UseConcMarkSweepGC
+  -XX:+ExplicitGCInvokesConcurrent}
 default["jetty"]["mongo_host"] = ""
 
 default["jetty"]["user"] = "jetty"
