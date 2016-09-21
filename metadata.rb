eaf7cfcacdc0e6ee5eca1396f71c08764664dd60
miguel_laptop <miguel.payares@tealium.com>
2016-09-21 11:00:46 -0700

    removed the apache license since this is jetty

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,6 @@
 name			       "jetty"
 maintainer       "Opscode, Inc."
 maintainer_email "devops@tealium.com"
-license          "Apache 2.0"
 description      "Installs/Configures jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.4.0"
