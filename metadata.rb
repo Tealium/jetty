6c210c36fe660108ad03bfe777f9ff2a3b250596
miguel_laptop <miguel.payares@tealium.com>
2016-09-21 10:58:27 -0700

    needed to update the metadata to reflect the newest changes

--- metadata.rb
+++ metadata.rb
@@ -1,16 +1,16 @@
-name			 "jetty"
+name			       "jetty"
 maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer_email "devops@tealium.com"
 license          "Apache 2.0"
 description      "Installs/Configures jetty"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.12"
+version          "0.4.0"
 
 %w{ java jpackage }.each do |cb|
   depends cb
 end
 
-%w{ debian ubuntu centos redhat fedora }.each do |os|
+%w{ debian ubuntu }.each do |os|
   supports os
 end
 
